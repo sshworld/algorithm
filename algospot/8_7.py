aa52b990283de25ebd1a4ba39927d940d5604587
sshworld <ssh97540@gmail.com>
2021-03-22 21:56:40 +0900

    PI

---
+++ algospot/8_7.py
@@ -0,0 +1,20 @@
+
+def pi() :
+    
+    return 
+
+def main() :
+
+    c = int(input())
+
+    for _ in range(c) :
+
+        numbers = list(map(int, input().split()))
+        
+
+
+
+
+
+
+main()
