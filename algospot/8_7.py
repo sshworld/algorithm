89adf06fb042480d8b61b53990c9b85b48591267
sshworld <ssh97540@gmail.com>
2021-03-23 18:55:31 +0900

    PI

--- algospot/8_7.py
+++ algospot/8_7.py
@@ -1,29 +1,89 @@
 
-def findLevel(dp) :
-    dp.append(5)
+def findLevel(num) :
 
-    return dp
+    # level 1
+    if(len(set(num)) == 1) :
+        return 1
 
-def start() :
+    prev = num[0] - 1
 
-    if
+    # level 2
+    for val in num :
+        if (val - prev != 1) :
+            break
+        
+        prev = val
+    else : 
+        return 2
+
+    prev = num[0] + 1
+
+    for val in num :
+        if (prev - val != 1) :
+            break
+        prev = val
+    else :
+        return 2
+
+    # level 3
+    twoNums = [num[0], num[1]]
+
+    ok = False
+
+    for val in num :
+        if(twoNums[ok] != val) :
+            break
+        ok = not ok
+    else :
+        return 4
+
+    #level 4
+    seq = num[1] - num[0]
+    prev = num[0] - seq
+
+    for val in num :
+        if(val != prev + seq) :
+            break
+        prev = val
+    else :
+        return 5
+
+    return 10
+
+def start(numbers, startNum, dp) :
+
+    n = len(numbers)
+
+    if(n == startNum) :
+        return 0
     
-    ref = float('inf')
+    if (dp[startNum]) :
+        return dp[startNum]
 
-    return ref
+    
+    ret = 99999
+
+    for i in range(3, 6) :
+        if (startNum + i <= n) :
+            ret = min(ret, start(numbers, startNum + i, dp) + findLevel(numbers[startNum:startNum+i]))
+
+
+    dp[startNum] = ret
+
+    return dp[startNum]
 
 def main() :
 
     c = int(input())
-    ans=[]
+    ans=[0 for _ in range(c)]
 
     for i in range(c) :
         
-        numbers = list(map(int, input().split()))
+        numbers = list(map(int, input()))
 
-        dp = []
+        dp = [0 for _ in range(len(numbers))]
 
-        ans.append(find(dp))
+        ans[i] = start(numbers, 0, dp)
 
     for q in ans :
         print(q)
