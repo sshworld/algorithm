05f01629fa3f8881c808f5a50c541573b242ea2b
sshworld <ssh97540@gmail.com>
2021-03-23 10:32:17 +0900

    PI

--- algospot/8_7.py
+++ algospot/8_7.py
@@ -1,20 +1,31 @@
 
-def pi() :
+def findLevel(dp) :
+    dp.append(5)
+
+    return dp
+
+def start() :
+
+    if
     
-    return 
+    ref = float('inf')
+
+    return ref
 
 def main() :
 
     c = int(input())
+    ans=[]
 
-    for _ in range(c) :
-
-        numbers = list(map(int, input().split()))
+    for i in range(c) :
         
+        numbers = list(map(int, input().split()))
 
+        dp = []
 
+        ans.append(find(dp))
 
-
-
+    for q in ans :
+        print(q)
 
 main()
