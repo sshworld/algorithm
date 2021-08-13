842526aecc3db5b898afe3df44be960561a66a31
sshworld <ssh97540@gmail.com>
2021-08-13 17:45:13 +0900

    insert sort & shell sort & merge sort

--- dataStructure/sort.py
+++ dataStructure/sort.py
@@ -21,19 +21,73 @@ def selectionSort(x) :
     start = time.time()
     for i in reversed(range(len(x))) :
         maxNum = 0
-        for j in range(i) :
+        for j in range(1, 1+i) :
             if x[j] > x[maxNum] :
                 maxNum = j
-        swap(x, maxNum, j)
+        swap(x, maxNum, i)
     print(time.time() - start)
     return x
 
 def insertSort(x) :
-    pass
+    start = time.time()
+    for i in range(1, len(x)) :
+        num = x[i]
+        j = i
+        while j > 0 and x[j-1] > num :
+            x[j] = x[j-1]
+            j -= 1
+        x[j] = num
+    print(time.time() - start)
+    return x
+
+def shellSort(x) :
+    start = time.time()
+    gap = len(x) // 2
+
+    while gap > 0 :
+        for i in range(gap) :
+            gapInsertionSort(x, i, gap)
+        gap = gap // 2
+    print(time.time() - start)
+    return x
 
 
+def gapInsertionSort(x, i , gap) :
+    for num in range(i + gap, len(x), gap) :
+        val = x[num]
+        targetNum = num
+        while targetNum > i :
+            if x[targetNum - gap] > val :
+                x[targetNum] = x[targetNum - gap]
+            else :
+                break
+            targetNum -= gap
+        x[targetNum] = val
+
+def mergeSort(x) :
+    if len(x) > 1 :
+        mid = len(x) // 2
+        lx, rx = x[:mid], x[mid:]
+        mergeSort(lx)
+        mergeSort(rx)
+    
+        li, ri, i = 0, 0, 0
+        while li < len(lx) and ri < len(rx) :
+            if lx[li] < rx[ri] :
+                x[i] = lx[li]
+                li += 1
+            else:
+                x[i] = rx[ri]
+                ri += 1
+            i += 1
+        x[i:] = lx[li:] if li != len(lx) else rx[ri:]
+    return(x)
 
 if __name__ == '__main__' :
-    x = [7, 1, 6, 2, 4, 3, 9, 10, 5]
-    print(bubbleSort(x))
-    print(selectionSort(x))
+    x = [10, 8, 6, 20, 4, 3, 22, 1, 0, 15, 16]
+    # print(bubbleSort(x))
+    # print(selectionSort(x))
+    # print(insertSort(x))
+    # print(shellSort(x))
+
+    print(mergeSort(x))
